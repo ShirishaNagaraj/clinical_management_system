66c5ab3dbb1fd2e514e998fce662e8fe1d4b31b6
SHIRISHA <shirisha445437@gmail.com>
2025-12-28 22:59:27 +0530

    Fix Base model and add audit fields

--- app/main.py
+++ app/main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI
+
 from app.db.session import lifespan
-import app.db.models
+import app.db.models  # 🔥 REQUIRED to create tables
+
+# Routers
+from app.routers.clinic_router import router as clinic_router
+from app.routers.doctor_router import router as doctor_router
+from app.routers.patient_router import router as patient_router
+from app.routers.appointment_router import router as appointment_router
+from app.routers.patient_document_router import router as document_router
 
 app = FastAPI(lifespan=lifespan)
 
+# Health check
 @app.get("/")
 async def root():
     return {"status": True, "message": "App running"}
+
+# Register routers
+app.include_router(clinic_router)
+app.include_router(doctor_router)
+app.include_router(patient_router)
+app.include_router(appointment_router)
+app.include_router(document_router)
+
