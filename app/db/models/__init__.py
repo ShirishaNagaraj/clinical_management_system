fa8639844cd35d53f7a710a8c5da4ca69118f409
SHIRISHA <shirisha445437@gmail.com>
2025-12-28 16:40:22 +0530

    Fix Base model and add audit fields

---
+++ app/db/models/__init__.py
@@ -0,0 +1,5 @@
+from app.db.models.clinic_model import Clinic
+from app.db.models.doctor_model import Doctor
+from app.db.models.patient_model import Patient
+from app.db.models.appointment_model import Appointment
+from app.db.models.patient_document_model import PatientDocument
