1c825dfa6ff9351e2148419219eaab90f1aae8f7
SHIRISHA <shirisha445437@gmail.com>
2025-12-28 20:38:36 +0530

    Fix Base model and add audit fields

--- app/exceptions/base_exception.py
+++ app/exceptions/base_exception.py
@@ -2,3 +2,7 @@ class AppException(Exception):
     def __init__(self, message: str, status_code: int = 400):
         self.message = message
         self.status_code = status_code
+
+class ValidationException(AppException):
+    def __init__(self, message: str):
+        super().__init__(message, 400)
