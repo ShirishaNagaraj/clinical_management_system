d1e6009b85ee44a5855a2e6179506e9b85640437
SHIRISHA <shirisha445437@gmail.com>
2025-12-28 21:19:56 +0530

    Fix Base model and add audit fields

--- app/core/security.py
+++ app/core/security.py
@@ -2,18 +2,21 @@ from datetime import datetime, timedelta
 from jose import jwt, JWTError
 from app.core.config import settings
 
+
 def create_access_token(data: dict):
     to_encode = data.copy()
+
     expire = datetime.utcnow() + timedelta(
         minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
     )
     to_encode.update({"exp": expire})
 
-    return jwt.encode(
+    token = jwt.encode(
         to_encode,
         settings.SECRET_KEY,
         algorithm=settings.ALGORITHM
     )
+    return token
 
 
 def decode_access_token(token: str):
